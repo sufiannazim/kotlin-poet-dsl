1c5546ef820d47fd7735f0c49ac589f8a7a6f6e3
sun12002 <sufian.nazim@uconn.edu>
2020-02-16 11:54:52 -0500

    project skeleton

---
+++ src/test/kotlin/com/github/kotlinpoetdsl/KotlinPoetDslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.kotlinpoetdsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPoetDslApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
